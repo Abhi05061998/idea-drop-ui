2949233a67fb8ce0b02bc3aea95a9cd16ffe0fef
Abhisek Chatterjee <abhisekchatterjee16@gmail.com>
2026-09-07 15:47:27 +0530

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -51,9 +51,7 @@ api.interceptors.response.use(
       } catch (err) {
         // If refresh fails, log out clean
         setStoredAccessToken(null)
-        if (window.location.pathname !== '/login') {
-          window.location.href = '/login'
-        }
+     
         return Promise.reject(err)
       }
     }
