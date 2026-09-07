aed7446b79c46b8672558e77d051cf4fc5351d4b
Abhisek Chatterjee <abhisekchatterjee16@gmail.com>
2026-09-07 15:03:56 +0530

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -17,8 +17,9 @@ type AuthContextType = {
 const AuthContext = createContext<AuthContextType | undefined>(undefined)
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [accessToken, setAccessToken] = useState<string | null>(null)
+  const [accessToken, setAccessToken] = useState<string | null>(() => getStoredAccessToken())
   const [user, setUser] = useState<AuthContextType['user'] | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   useEffect(() => {
     const loadAuth = async () => {
@@ -29,6 +30,9 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         setUser(user)
       } catch (error) {
         console.log('Failed to refresh access token:', error)
+      }
+      finally {
+        setIsLoading(false) // 👈 Session check finished
       }
     }
     loadAuth()
