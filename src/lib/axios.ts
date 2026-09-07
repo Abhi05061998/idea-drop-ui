fda1762aa2e6adff272fbdd290ee6f47a1aa8525
Abhi05061998 <abhisekchatterjee16@gmail.com>
2026-09-07 10:52:13 +0530

    changed url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getStoredAccessToken, setStoredAccessToken } from './authToken'
 import { refreshAccessToken } from '../apis/auth'
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_PRODUCTION_URL}/api`,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
